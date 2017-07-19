40349b06935cdb0ae26177d54470a34e59052c67
Catherine Noel <catherinemnoel@gmail.com>
2017-07-19 17:02:52 -0400

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+var rugratsTriviaGame = {
+
+	triviaQandA: {
+		q1: {
+			question: "Who's last name is Finster?",
+			a: "Spike",
+			b: "Phil",
+			c: "Tommy",
+			d: "Chuckie",
+			picture: "../images/chuckieq1.jpg"
+		},
+		q2: {
+			question: "Tommy and Angelica are..."
+			a: "Friends",
+			b: "Brother and sister",
+			c: "Cousins",
+			d: "Neighbors"
+			picture: "../images/angelica&tommyq2.jpeg"
+		},
+	},
+};
