c1a729ab5ef54514f6d39ce0f35d4e33aaa02efb
Catherine Noel <catherinemnoel@gmail.com>
2017-07-20 19:19:14 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,12 +10,47 @@ var rugratsTriviaGame = {
 			picture: "../images/chuckieq1.jpg"
 		},
 		q2: {
-			question: "Tommy and Angelica are..."
+			question: "Tommy and Angelica are...",
 			a: "Friends",
 			b: "Brother and sister",
 			c: "Cousins",
-			d: "Neighbors"
+			d: "Neighbors",
 			picture: "../images/angelica&tommyq2.jpeg"
 		},
+		q3: {
+			question: "What is Angelica's doll's name?",
+			a: "Cindy",
+			b: "Jessica",
+			c: "Cynthia",
+			d: "Elizabeth",
+			picture: "../images/cynthiadoll.jpg"
+		},
+		q4: {
+			question: "Who are the pair of twins?",
+			a: "Chuckie and Luckie",
+			b: "Dill and Phil",
+			c: "Dill and Lil",
+			d: "Phil and Lil",
+			picture: "../images/philandlil.jpg"
+		},
+		q5: {
+			question: "Tommy's dad job is...",
+			a: "Inventer",
+			b: "Zookeeper",
+			c: "Vet",
+			d: "Doctor",
+			picture: "../images/stupickles.png"
+		}
 	},
+
+	//start game
+	startGame: function() {
+		console.log("hey")
+	},
+
+
 };
+
+
+
+
